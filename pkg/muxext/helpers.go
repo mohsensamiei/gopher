f0fa72fc8ab37c09375e947c4266cfbfcb769315
Mohsen Samiei <mohsen.samiei@podro.com>
2023-03-04 16:37:22 +0330

    feat: get key from param

--- pkg/muxext/helpers.go
+++ pkg/muxext/helpers.go
@@ -1,12 +1,28 @@
 package muxext
 
 import (
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func NewRouter() *mux.Router {
 	return mux.NewRouter().StrictSlash(true)
+}
+
+type key interface {
+	SetID(id uuid.UUID)
+	SetSlug(slug string)
+}
+
+func PathKey(req *http.Request, name string, out key) {
+	v := mux.Vars(req)[name]
+	id, err := uuid.Parse(v)
+	if err == nil {
+		out.SetID(id)
+		return
+	}
+	out.SetSlug(v)
 }
 
 func PathParam(req *http.Request, name string) string {
