f50bb1ae0d4dea3094a3f2e34d766e3f5dbe30ce
Mohsen Samiei <mohsen.samiei@podro.com>
2023-03-04 16:38:51 +0330

    fix: pointer zero value

--- pkg/pointers/extention.go
+++ pkg/pointers/extention.go
@@ -1,9 +1,17 @@
 package pointers
 
-func ToValue[T any](s *T) T {
+func ToValue[T comparable](s *T) T {
+	if s == nil {
+		var zero T
+		return zero
+	}
 	return *s
 }
 
-func ToPointer[T any](s T) *T {
+func ToPointer[T comparable](s T) *T {
+	var zero T
+	if s == zero {
+		return nil
+	}
 	return &s
 }
