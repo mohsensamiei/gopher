b1a0f1141256cf77041b3ca651fafc777745443f
Mohsen Samiei <mohsen.samiei@podro.com>
2023-03-04 16:25:10 +0330

    feat: gorm transaction

---
+++ pkg/gormext/transaction.go
@@ -0,0 +1,22 @@
+package gormext
+
+import (
+	"context"
+	"github.com/pinosell/gopher/pkg/di"
+	"gorm.io/gorm"
+)
+
+func Transaction(ctx context.Context, f func(ctx context.Context) error) (err error) {
+	db := di.Provide[*gorm.DB](ctx, Name)
+	tx := db.Begin().WithContext(ctx)
+	defer func() {
+		if err != nil {
+			_ = tx.Rollback()
+		}
+	}()
+	sub := context.WithValue(ctx, Name, tx)
+	if err = f(sub); err != nil {
+		return err
+	}
+	return tx.Commit().Error
+}
