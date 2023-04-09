e964e37afbc7d258364cef739745549627d74357
Mohsen Samiei <mohsen.samiei@podro.com>
2023-04-10 02:42:42 +0330

    feat: add grom di http middleware

--- pkg/gormext/di.go
+++ pkg/gormext/di.go
@@ -2,9 +2,21 @@ package gormext
 
 import (
 	"context"
+	"github.com/gorilla/mux"
 	"google.golang.org/grpc"
 	"gorm.io/gorm"
+	"net/http"
 )
+
+func DIMiddleware(db *gorm.DB) mux.MiddlewareFunc {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+			ctx := req.Context()
+			ctx = context.WithValue(ctx, Name, db.WithContext(ctx))
+			next.ServeHTTP(res, req.WithContext(ctx))
+		})
+	}
+}
 
 func UnaryDIInterceptor(db *gorm.DB) grpc.UnaryServerInterceptor {
 	return func(
