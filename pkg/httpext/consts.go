77a83b941d54b474413d1c5b827aebd29039cef6
Mohsen Samiei <mohsen.samiei@podro.com>
2023-04-10 02:43:36 +0330

    feat: add some http header methods

--- pkg/httpext/consts.go
+++ pkg/httpext/consts.go
@@ -1,9 +1,11 @@
 package httpext
 
 const (
-	Platform            = "HTTP"
-	ContentTypeHeader   = "Content-Type"
-	CharsetHeader       = "Accept-Charset"
-	AcceptHeader        = "Accept"
-	AuthorizationHeader = "Authorization"
+	Platform             = "HTTP"
+	ContentTypeHeader    = "Content-Type"
+	CharsetHeader        = "Accept-Charset"
+	AcceptHeader         = "Accept"
+	AuthorizationHeader  = "Authorization"
+	RealIPHeader         = "X-Real-IP"
+	AcceptLanguageHeader = "Accept-Language"
 )
--- pkg/httpext/header.go
+++ pkg/httpext/header.go
@@ -3,5 +3,9 @@ package httpext
 import "net/http"
 
 func RealIP(req *http.Request) string {
-	return req.Header.Get("X-Real-IP")
+	return Header(req, RealIPHeader)
+}
+
+func Header(req *http.Request, key string) string {
+	return req.Header.Get(key)
 }
