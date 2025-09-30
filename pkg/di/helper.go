96a68bc4789936c2ec1dd493248cd7cfbeaaeb7e
Mohsen Samiei <mohsen.samiei@podro.com>
2025-09-30 16:07:29 +0330

    fix: di type name

--- pkg/di/helper.go
+++ pkg/di/helper.go
@@ -1,17 +1,33 @@
 package di
 
 import (
+	"fmt"
 	"reflect"
 )
 
 func TypeName[T any]() string {
-	var x T
-	t := reflect.TypeOf(x)
-	if t == nil {
-		t = reflect.TypeOf((*T)(nil)).Elem()
+	t := reflect.TypeOf((*T)(nil)).Elem()
+	return typeString(t)
+}
+
+func typeString(t reflect.Type) string {
+	switch t.Kind() {
+	case reflect.Ptr:
+		return "*" + typeString(t.Elem())
+	case reflect.Slice:
+		return "[]" + typeString(t.Elem())
+	case reflect.Array:
+		return fmt.Sprintf("[%d]%s", t.Len(), typeString(t.Elem()))
+	case reflect.Map:
+		return "map[" + typeString(t.Key()) + "]" + typeString(t.Elem())
+	case reflect.Chan:
+		return "chan " + typeString(t.Elem())
+	case reflect.Func:
+		return "func"
+	default:
+		if t.PkgPath() == "" {
+			return t.Name()
+		}
+		return t.PkgPath() + "." + t.Name()
 	}
-	if t.PkgPath() == "" {
-		return t.Name()
-	}
-	return t.PkgPath() + "." + t.Name()
 }
